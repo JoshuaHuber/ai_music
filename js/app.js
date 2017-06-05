5946c2ca9b215a5c7c1b01430339f96cff23388a
fastexposure <fastexposure@yahoo.com>
2017-06-05 14:53:07 -0700

    added voice play/pause/next/last voice recognition

--- js/app.js
+++ js/app.js
@@ -4,18 +4,21 @@ function init(){
     SC.initialize({
       client_id: soundCloudClientID
     });
+    annyang.abort();
 }
 var iframeElement   = document.querySelector('iframe');
 var iframeElementID = iframeElement.id;
 var widget1         = SC.Widget(iframeElement);
 var widget2         = SC.Widget(iframeElementID);
-
+var state = true;
 // get input from user...
 function getUserInput() {
     $("#js-userSubmit").submit(function(event) {
         event.preventDefault();
         var userInput = $("#js-userInput").val();
-        getSoundCloud(userInput);
+        if(userInput.length > 1){
+            getSoundCloud(userInput);
+        }
     })
 }
 function getSoundCloud(userInput){
@@ -23,19 +26,24 @@ function getSoundCloud(userInput){
     SC.get('/users', {
         q: userInput
     }).then(function(users) {
-        if(users){
-            var userUrl = users[0].uri;
-            loadSoundCloud(userUrl);
+        if(users.length > 0){
+            console.log(users);
+            for(var i = 0; i < users.length; i++){
+                if( users[i].track_count > 5 && users[i].followers_count > 10 && users[i].track_count > 5){
+                    var userUrl = users[i].uri;
+                    loadSoundCloud(userUrl);
+                    console.log(users[i]);
+                    return;
+                }
+            }
         } else {
-            //TODO add else
+            $('.noSongs').toggleClass('hide')
         }
-
     });
 }
 function loadSoundCloud(userUrl){
-    widget1.load(userUrl, {auto_play: true});
+    widget1.load(userUrl, {auto_play: true, limit: 10});
 }
-var state = true;
 if (annyang) {
   // Add our commands to annyang
      annyang.addCommands({
@@ -44,8 +52,33 @@ if (annyang) {
             $('#js-userInput').val(artist);
             getSoundCloud(artist);
         },
+        'play': function() {
+            widget1.play();
+        },
+        'pause': function() {
+            widget1.pause();
+        },
+        'next *song': function() {
+            widget1.next();
+        },
+        'next': function() {
+            widget1.next();
+        },
+        'previous *song': function() {
+            widget1.prev();
+        },
+        'previous': function() {
+            widget1.prev();
+        },
+        'last *song': function() {
+            widget1.prev();
+        },
+        'last': function() {
+            widget1.prev();
+        }
     });
     $("#mic").click(function() {
+        $(".micFA").toggleClass('record');
          if(state === true) {
             annyang.start();
             annyang.resume();
